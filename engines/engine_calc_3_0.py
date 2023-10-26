8f8f9738665d4a7fc443d90a4d53cc41263be3b4
Rosamon <klimroman2001@mail.ru>
2023-10-26 21:54:22 +0300

    Fixed engine

--- engines/engine_calc_3_0.py
+++ engines/engine_calc_3_0.py
@@ -1,6 +1,5 @@
 import socket
 import json
-import select
 import multiprocessing
 import threading
 import time
@@ -8,17 +7,20 @@ from ctypes import *
 
 md5 = CDLL('./ecalc_md5.so')
 
-host = socket.gethostname() # as both code is running on same pc
-#host = '10.1.1.209'
+#host = socket.gethostname() # as both code is running on same pc
+host = '10.1.1.43'
 #port = 5001  # socket server port number
 SOURCE_PORT, DESTINATION_PORT = 6666, 9090
-characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-kill_phrase = 'SIGKILL'
+#characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+kill_phrase = 'Interrupt'
 
 def return_proc_res(queue, func, num, req_hash):
     #print("Process of Python get res md5 - ", multiprocessing.current_process())
     try:
-        queue.put(func(c_int(num), c_char_p(str.encode(req_hash))))
+        print("The calculations have begun.")
+        r = func(c_int(num), c_char_p(str.encode(req_hash)))
+        queue.put(r)
+        print("The calculations ended. Result - ", r.decode('utf-8'))
     except Exception as e:
         print("proc_res error")
         queue.put(0)
@@ -29,12 +31,12 @@ def return_proc_res(queue, func, num, req_hash):
 
 def listen_socket_state(queue, some_socket):
     try:
-        #print("Process of Python socket state started ")
+        print("Process of listening socket started ")
         r = some_socket.recv(1024)
         while not queue.empty():
-            time.sleep(0.)
+            time.sleep(0.1)
         queue.put(r)
-        print("Process of Python socket state ended ", r.decode('utf-8'))
+        print("Process of listening socket ended. Received - ", r.decode('utf-8'))
     except Exception as e:
         #print("Process of Python socket state error", e)
         pass
@@ -47,18 +49,18 @@ def client_program(queue_flag):
     req_hash = ""
 
     try:
-        client_socket.bind(('0.0.0.0', SOURCE_PORT))
+        #client_socket.bind(('0.0.0.0', SOURCE_PORT))
         client_socket.connect((host, DESTINATION_PORT))  # connect to the server
         data = client_socket.recv(1024)
         parameters = json.loads(data)  # receive response
         number = int(parameters['number'])
         req_hash = parameters['hash']
-        print("connection ok. number - ", number, " hash - ", req_hash)
+        print("Connection established. number - ", number, " hash - ", req_hash)
 
     except Exception as e:
         client_socket.close()
         time.sleep(0.5)
-        print(e)
+        print(e, "connect")
         queue_flag.put(0)
         return
 
@@ -76,27 +78,32 @@ def client_program(queue_flag):
         listen_SIGKILL_thread.start()
         ########################################################
 
-        res = queue.get().decode()
+        res = queue.get().decode('utf-8')
+        #or kill phrase or answer
+        #print(res, ' ', res == kill_phrase)
         if (res != "None") or (res == kill_phrase):
             if res == kill_phrase:
                 proc.terminate()
                 queue_flag.put(0)
+                client_socket.close()
                 return
             flag = not flag
 
         try:
             client_socket.send(res.encode())
-            print("sended data - ", res)
+            print("The answer is sended to the manager.")
         except Exception as e:
             break
         #client_socket.send(res)
         proc.terminate()
         #print("Process of Python client program - ", multiprocessing.current_process())
+        #print(f"{flag}")
         if flag:
             # if queue.empty():
+            #print("Q: ")
             msg = queue.get()
             #wait dor number data or interrupt signal
-            print(msg)
+            #print("Q: ",msg)
             if msg != kill_phrase:
                 number = int(msg)
                 #flag = not flag
@@ -106,7 +113,7 @@ def client_program(queue_flag):
                 return
 
     client_socket.close()
-    print("conn close")
+    print("Connection closed.")
     queue_flag.put(0)
     return
 
